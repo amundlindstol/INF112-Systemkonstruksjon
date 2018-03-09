4d4af4f45f2f70fd07a3bbc3a08cdc21f0bcd7bf
eivind.halderaker@student.uib.no <GitCommitPassword>
2018-03-09 11:27:07 +0100

    Make Game.java skeleton

---
+++ core/src/com/grnn/chess/Game.java
@@ -0,0 +1,24 @@
+package com.grnn.chess;
+
+public class Game {
+
+    private Board board;
+    private Player player1;
+    private Player player2;
+    private String name;
+
+
+    private void startGame() {
+
+    }
+
+    private void endGame(Result res) {
+
+    }
+
+    private Player announceWinner() {
+
+    }
+
+
+}
