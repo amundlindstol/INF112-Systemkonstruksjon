b9e2e26b1482c577a535eadbe84c7318922ba1de
simon riple <sri029@uib.no>
2018-03-11 23:46:09 +0100

    Started working on the player class

---
+++ core/src/com/grnn/chess/player.java
@@ -0,0 +1,38 @@
+public class Player {
+
+    public String name;
+    public String level;
+    public int rating;
+    private int noOfWins, noOfLose, noOfDraws;
+    public ArrayList<Game> gamesPlayed;
+
+    public Player(String name){
+        this.name = name;
+        gamesPlayed = new ArrayList<Game>;
+        noOfWins = 0;
+        noOfLose = 0;
+        noOfDraws = 0;
+        updaterating
+    }
+
+    public int getLevel(){
+        return level;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void updateStatictics(Player otherPlayer, int score){
+        int k;
+        if(gamesPlayed.size() < 30){
+            k = 40;
+        }else if(rating < 2400){
+            k = 20
+        }else {
+            k = 40;
+        }
+        rating = rating +
+    }
+
+}
