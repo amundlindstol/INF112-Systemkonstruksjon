b4ea66801b2e85a682a06575e6b14cb7de046dfa
Helge Mikael Landro <helge.landro@student.uib.no>
2018-03-12 21:44:48 +0100

    Changed bg color and image for the display window

--- core/src/com/grnn/chess/ChessForKids.java
+++ core/src/com/grnn/chess/ChessForKids.java
@@ -13,15 +13,15 @@ public class ChessForKids extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture(Gdx.files.internal("badlogic.jpg"));
+		img = new Texture(Gdx.files.internal("sjakk2.png"));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 1, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 200, 200);
+		batch.draw(img, 200, 50);
 		batch.end();
 	}
 	
