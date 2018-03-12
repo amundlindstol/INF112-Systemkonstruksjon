66c9196c6479a673078a0c8bf0bb861af387b46e
Helge Mikael Landro <helge.landro@student.uib.no>
2018-03-12 21:35:01 +0100

    Changed size for the display window

--- desktop/src/com/grnn/chess/desktop/DesktopLauncher.java
+++ desktop/src/com/grnn/chess/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.grnn.chess.ChessForKids;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1000;
+		config.height = 700;
 		new LwjglApplication(new ChessForKids(), config);
 	}
 }
