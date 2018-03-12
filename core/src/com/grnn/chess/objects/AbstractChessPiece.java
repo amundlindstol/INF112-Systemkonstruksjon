4f39e846899fdaa7b794fd6033e9b95256b41f34
Helge Mikael Landro <helge.landro@student.uib.no>
2018-03-12 13:25:42 +0100

    Update on AbstractChessPiece

--- core/src/com/grnn/chess/objects/AbstractChessPiece.java
+++ core/src/com/grnn/chess/objects/AbstractChessPiece.java
@@ -3,12 +3,14 @@ package com.grnn.chess.objects;
 public abstract class AbstractChessPiece {
     private boolean isWhite;
     private boolean validMove;
+    //private Direction askedToGo;
 
     /*public Position getPosition(){
-        return null;
+
     }*/
 
     public boolean getColor(){
+
         return isWhite;
     }
 }
