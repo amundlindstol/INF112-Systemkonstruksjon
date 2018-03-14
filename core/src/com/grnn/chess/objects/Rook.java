5fd712abc9ca032d97623144dea952bba2d21f7e
Agnete Gridseth Røstad <agnete.rostad@student.uib.no>
2018-03-14 15:55:31 +0100

    Did getValidMoves

--- core/src/com/grnn/chess/objects/Rook.java
+++ core/src/com/grnn/chess/objects/Rook.java
@@ -20,8 +20,49 @@ public class Rook extends AbstractChessPiece {
 
 	//TODO: actually implement this
 	public ArrayList<Position> getValidMoves(Board board) {
+		ArrayList<Position> validMoves = new ArrayList<Position>();
+
+		//Get the position of the rook
+		Position rookPos = getPosition(board);
+		System.out.println(rookPos.getX()+","+rookPos.getY()+" "+this.isWhite);
+
+		int i=1;
+
+		do {
+		    if (board.posIsWithinBoard(rookPos.west(i)) {
+                validMoves.add(rookPos.west(i));
+                ++i;
+		    }
+		    else break;
+		} while (board.getPieceAt(rookPos.west(i))==null);
+		i = 0;
+
+        do { if (board.posIsWithinBoard(rookPos.east(i)) {
+                validMoves.add(rookPos.east(i));
+                ++i;
+            }
+            else break;
+        } while (board.getPieceAt(rookPos.east(i))==null);
+		i = 0;
+
+        do { if board.posIsWithinBoard(rookPos.north(i)) {
+                validMoves.add(rookPos.north(i));
+                ++i;
+            }
+            else break;
+        } while (board.getPieceAt(rookPos.north(i))==null);
+        i = 0;
+
+        do { if board.posIsWithinBoard(rookPos.north(i)) {
+                validMoves.add(rookPos.south(i));
+                ++i
+            }
+            else break;
+        } while (board.getPieceAt(rookPos.south(i))==null);
+
 		return (ArrayList<Position>) Arrays.asList(board.getPosition(this));
 	}
+
 
 	//TODO: actually implement this
 	public ArrayList<Position> getCaptureMoves(Board board) {
