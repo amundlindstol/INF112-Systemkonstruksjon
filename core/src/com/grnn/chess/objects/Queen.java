a8bebad1e91858d502841e90902ce219729e1723
eivind.halderaker@student.uib.no <GitCommitPassword>
2018-03-15 13:03:46 +0100

    started validmoves Queen

--- core/src/com/grnn/chess/objects/Queen.java
+++ core/src/com/grnn/chess/objects/Queen.java
@@ -1,5 +1,10 @@
 package com.grnn.chess.objects;
 
+import com.grnn.chess.Board;
+import com.grnn.chess.Position;
+
+import java.util.ArrayList;
+
 public class Queen extends AbstractChessPiece{
 	private final int value = 9;
 	String letterRepresentation = "q";
@@ -14,7 +19,33 @@ public class Queen extends AbstractChessPiece{
 
 	public String getImage(){ return image;}
 
-	public int getValue() {
+	public ArrayList<Position> getValidMoves(Board board) {
+		Position pawnPos = getPosition(board);
+		System.out.println(pawnPos.getX()+","+pawnPos.getY()+" "+this.isWhite);
+
+		if(isWhite){
+
+		}else {
+
+		}
+		return null;
+	}
+
+	public ArrayList<Position> getValidMovesWest(Board board) {
+		Position pawnPos = getPosition(board);
+
+		int i = 1;
+		do {
+			if (board.posIsWithinBoard(pawnPos.west(i)) && !isSameColor(board.getPieceAt(pawnPos.west(i)))){
+				validMoves.add(pawnPos.west(i));
+			}
+			else break;
+		} while (board.getPieceAt(pawnPos.west(i++))==null);
+
+	}
+
+
+		public int getValue() {
 		return value;
 	}
 }
