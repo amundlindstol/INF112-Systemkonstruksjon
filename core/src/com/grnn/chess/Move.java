df252a96e56300bc4e12b930742be49a737e4cbf
eivind.halderaker@student.uib.no <GitCommitPassword>
2018-03-15 12:00:47 +0100

    Make class for moves

--- core/src/com/grnn/chess/Move.java
+++ core/src/com/grnn/chess/Move.java
@@ -1,4 +1,17 @@
 package com.grnn.chess;
 
+import com.grnn.chess.objects.AbstractChessPiece;
+
 public class Move {
+
+    private Position toPos;
+    private Position fromPos;
+    private AbstractChessPiece piece;
+
+    public Move(Position toPos, Position fromPos, AbstractChessPiece piece) {
+        toPos = toPos;
+        fromPos = fromPos;
+        piece = piece;
+    }
+
 }
