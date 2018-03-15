2e294aba0eb266dbf6571ff1f8768a8fdbdda96d
Agnete Gridseth Røstad <agnete.rostad@student.uib.no>
2018-03-15 14:03:38 +0100

    Added getValidMoves method to Queen

--- core/src/com/grnn/chess/objects/Queen.java
+++ core/src/com/grnn/chess/objects/Queen.java
@@ -1,5 +1,10 @@
 package com.grnn.chess.objects;
 
+import com.grnn.chess.Board;
+import com.grnn.chess.Position;
+
+import java.util.ArrayList;
+
 public class Queen extends AbstractChessPiece{
 	private final int value = 9;
 	String letterRepresentation = "q";
@@ -11,6 +16,88 @@ public class Queen extends AbstractChessPiece{
 	public String toString() {
 		return isWhite ? letterRepresentation : letterRepresentation.toUpperCase();
 	}
+
+	public ArrayList<Position> getValidMoves(Board board) {
+		ArrayList<Position> validMoves = new ArrayList<Position>();
+
+		Position queenPos = getPosition(board);
+		System.out.println(queenPos.getX()+","+queenPos.getY()+" "+this.isWhite);
+		validMoves.addAll(getValidVerticalAndHorizontalMoves(board, queenPos));
+		validMoves.addAll(getValidDiagonalMoves(board,queenPos));
+		return validMoves;
+
+	}
+	public ArrayList<Position> getValidVerticalAndHorizontalMoves(Board board, Position queenPos){	//Get the position of the rook
+		ArrayList<Position> validMoves = new ArrayList<Position>();
+		int i=1;
+
+		do {
+			if (board.posIsWithinBoard(queenPos.west(i)) && !isSameColor(board.getPieceAt(queenPos.west(i)))){
+				validMoves.add(queenPos.west(i));
+			}
+			else break;
+		} while (board.getPieceAt(queenPos.west(i++))==null);
+		i = 1;
+
+		do { if (board.posIsWithinBoard(queenPos.east(i)) && !isSameColor(board.getPieceAt(queenPos.east(i)))){
+			validMoves.add(queenPos.east(i));
+		}
+		else break;
+		} while (board.getPieceAt(queenPos.east(i++))==null);
+		i = 1;
+
+		do { if (board.posIsWithinBoard(queenPos.north(i)) && !isSameColor(board.getPieceAt(queenPos.north(i)))){
+			validMoves.add(queenPos.north(i));
+		}
+		else break;
+		} while (board.getPieceAt(queenPos.north(i++))==null);
+		i = 1;
+
+		do { if (board.posIsWithinBoard(queenPos.south(i)) && !isSameColor(board.getPieceAt(queenPos.south(i)))){
+			validMoves.add(queenPos.south(i));
+		}
+		else break;
+		} while (board.getPieceAt(queenPos.south(i++))==null);
+
+		return validMoves;
+	}
+
+	public ArrayList<Position> getValidDiagonalMoves(Board board, Position queenPos) {
+		ArrayList<Position> validMoves = new ArrayList<Position>();
+
+		int i=1;
+
+		do {
+			if (board.posIsWithinBoard(queenPos.west(i).north(i)) && !isSameColor(board.getPieceAt(queenPos.west(i).north(i)))){
+				validMoves.add(queenPos.west(i));
+			}
+			else break;
+		} while (board.getPieceAt(queenPos.west(i).north(i++))==null);
+		i = 1;
+
+		do { if (board.posIsWithinBoard(queenPos.east(i).north(i)) && !isSameColor(board.getPieceAt(queenPos.east(i).north(i)))){
+			validMoves.add(queenPos.east(i));
+		}
+		else break;
+		} while (board.getPieceAt(queenPos.east(i).north(i++))==null);
+		i = 1;
+
+		do { if (board.posIsWithinBoard(queenPos.west(i).south(i)) && !isSameColor(board.getPieceAt(queenPos.west(i).south(i)))){
+			validMoves.add(queenPos.west(i).south(i));
+		}
+		else break;
+		} while (board.getPieceAt(queenPos.west(i).south(i++))==null);
+		i = 1;
+
+		do { if (board.posIsWithinBoard(queenPos.east(i).south(i)) && !isSameColor(board.getPieceAt(queenPos.east(i).south(i)))){
+			validMoves.add(queenPos.east(i).south(i));
+		}
+		else break;
+		} while (board.getPieceAt(queenPos.east(i).south(i++))==null);
+
+		return validMoves;//(ArrayList<Position>) Arrays.asList(board.getPosition(this));
+	}
+
 
 	public String getImage(){ return image;}
 
