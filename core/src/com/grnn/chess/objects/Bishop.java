800e49272d70a48267087f38000209badf97ef4b
eivind.halderaker@student.uib.no <GitCommitPassword>
2018-03-19 18:33:19 +0100

    Remade validMoves() method

--- core/src/com/grnn/chess/objects/Bishop.java
+++ core/src/com/grnn/chess/objects/Bishop.java
@@ -6,6 +6,7 @@ import com.grnn.chess.Position;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 
 public class Bishop extends AbstractChessPiece {
 	String letterRepresentation = "b";
@@ -22,41 +23,66 @@ public class Bishop extends AbstractChessPiece {
 		ArrayList<Position> validMoves = new ArrayList<Position>();
 
 		//Get the position of the rook
-		Position bishopPos = getPosition(board);
+		validMoves.addAll(getValidMovesNorthWest(board));
+		validMoves.addAll(getValidMovesNorthEast(board));
+		validMoves.addAll(getValidMovesSouthWest(board));
+		validMoves.addAll(getValidMovesSouthEast(board));
 
-		int i=1;
-
-		do {
-			if (board.posIsWithinBoard(bishopPos.west(i).north(i)) && !isSameColor(board.getPieceAt(bishopPos.west(i).north(i)))){
-				validMoves.add(bishopPos.west(i));
-			}
-			else break;
-		} while (board.getPieceAt(bishopPos.west(i).north(i++))==null);
-		i = 1;
-
-		do { if (board.posIsWithinBoard(bishopPos.east(i).north(i)) && !isSameColor(board.getPieceAt(bishopPos.east(i).north(i)))){
-			validMoves.add(bishopPos.east(i));
-		}
-		else break;
-		} while (board.getPieceAt(bishopPos.east(i).north(i++))==null);
-		i = 1;
-
-		do { if (board.posIsWithinBoard(bishopPos.west(i).south(i)) && !isSameColor(board.getPieceAt(bishopPos.west(i).south(i)))){
-			validMoves.add(bishopPos.west(i).south(i));
-		}
-		else break;
-		} while (board.getPieceAt(bishopPos.west(i).south(i++))==null);
-		i = 1;
-
-		do { if (board.posIsWithinBoard(bishopPos.east(i).south(i)) && !isSameColor(board.getPieceAt(bishopPos.east(i).south(i)))){
-			validMoves.add(bishopPos.east(i).south(i));
-		}
-		else break;
-		} while (board.getPieceAt(bishopPos.east(i).south(i++))==null);
-
-		return validMoves;//(ArrayList<Position>) Arrays.asList(board.getPosition(this));
+		return validMoves;
 	}
 
+	private ArrayList<Position> getValidMovesSouthEast(Board board) {
+		ArrayList<Position> validMoves = new ArrayList<Position>();
+		Position bishopPos = getPosition(board);
+
+		Position southEast = bishopPos.south().east();
+		while(board.posIsWithinBoard(southEast) && board.getPieceAt(southEast) == null) {
+			validMoves.add(southEast);
+			southEast = southEast.south().east();
+		}
+
+		return validMoves;
+
+	}
+
+	private ArrayList<Position> getValidMovesSouthWest(Board board) {
+		ArrayList<Position> validMoves = new ArrayList<Position>();
+		Position bishopPos = getPosition(board);
+
+		Position southWest = bishopPos.south().west();
+		while(board.posIsWithinBoard(southWest) && board.getPieceAt(southWest) == null) {
+			validMoves.add(southWest);
+			southWest = southWest.south().west();
+		}
+		return validMoves;
+	}
+
+	private ArrayList<Position> getValidMovesNorthEast(Board board) {
+		ArrayList<Position> validMoves = new ArrayList<Position>();
+		Position bishopPos = getPosition(board);
+
+		Position northEast = bishopPos.north().east();
+		while(board.posIsWithinBoard(northEast) && board.getPieceAt(northEast) == null) {
+			validMoves.add(northEast);
+			northEast = northEast.north().east();
+		}
+		return validMoves;
+	}
+	
+
+	private ArrayList<Position> getValidMovesNorthWest(Board board) {
+		ArrayList<Position> validMoves = new ArrayList<Position>();
+		Position bishopPos = getPosition(board);
+
+		Position northWest = bishopPos.north().west();
+		while(board.posIsWithinBoard(northWest) && board.getPieceAt(northWest) == null) {
+			validMoves.add(northWest);
+			northWest = northWest.north().west();
+		}
+
+		return validMoves;
+	}
+	/*
 	public boolean willKingBePutInCheckByMoveTo(Board board, AbstractChessPiece king, Position pos){
         Position posToCheck = pos.north(1).west((1));
         while (board.posIsWithinBoard(posToCheck)){
@@ -99,7 +125,9 @@ public class Bishop extends AbstractChessPiece {
         }
 
         return false;
+
     }
+        */
 
 	//TODO: actually implement this
 	public ArrayList<Position> getCaptureMoves(Board board) {
