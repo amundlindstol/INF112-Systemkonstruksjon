fdc7ca2bfbe6a63e8761b3bcb97491eb06686ebc
Håkon Osland <hakon.ettestol.osland@gmail.com>
2018-03-19 16:15:35 +0100

    removed Eivinds toString in Position

--- core/src/com/grnn/chess/Position.java
+++ core/src/com/grnn/chess/Position.java
@@ -23,10 +23,6 @@ public class Position implements Comparable<Object>{
 		return posY;
 	}
 
-	public String toString() {
-		return "(" + posX + ", " + posY + ")";
-	}
-
 	//Returns a new position 'steps' tiles further north
 	public Position north(int steps) { return new Position(posX,posY+steps); }
 
@@ -63,9 +59,8 @@ public class Position implements Comparable<Object>{
 			return true;
 		return false;
 	}
-
-	@Override
+	
 	public String toString() {
-		return "x: " + posX + ", y: " + posY;
+		return "(" + posX + ", " + posY + ")";
 	}
 }
