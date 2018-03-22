592a23a0ecde35d505d9bd82142c62cee29e5673
simon riple <sri029@uib.no>
2018-03-22 15:08:06 +0100

    En passant

--- core/src/com/grnn/chess/Board.java
+++ core/src/com/grnn/chess/Board.java
@@ -224,18 +224,20 @@ public class Board {
             Position conditionFromPos = conditionMove.getFromPos();
             Position conditionToPos = conditionMove.getToPos();
             AbstractChessPiece conditionPiece = conditionMove.getPiece();
+            if(lastPiece.getClass() == Pawn.class && conditionPiece.getClass() == Pawn.class) {
+                if (conditionToPos.getX() == lastToPos.getX()) {
+                    if (lastPiece.isWhite() && lastFromPos.getY() == 4 && lastToPos.getY() == 5) {
+                        if (!conditionPiece.isWhite() && conditionFromPos.getY() == 6 && conditionToPos.getY() == 4) {
+                            setPiece(null, conditionToPos);
+                        }
+                    } else if (lastFromPos.getY() == 3 && lastFromPos.getY() == 2) {
+                        if (conditionPiece.isWhite() && conditionFromPos.getY() == 1 && conditionToPos.getY() == 3) {
+                            setPiece(null, conditionToPos);
+                        }
 
-            if (conditionToPos.getX() == lastToPos.getX())
-                if (lastPiece.isWhite() && lastFromPos.getY() == 4 && lastToPos.getY() == 5) {
-                    if (!conditionPiece.isWhite() && conditionFromPos.getY() == 6 && conditionToPos.getY() == 4) {
-                        setPiece(null, conditionToPos);
                     }
-                } else {
-                    if (conditionPiece.isWhite() && conditionFromPos.getY() == 1 && conditionToPos.getY() == 3) {
-                        setPiece(null, conditionToPos);
-                    }
-
                 }
+            }
         }
     }
 }
