3845b82bbd46f5d65feea9bc0e0c51a0f096071a
Helge Mikael Landro <helge.landro@student.uib.no>
2018-04-19 12:08:18 +0200

    Statistics are available

--- core/src/com/grnn/chess/states/ShowStatsState.java
+++ core/src/com/grnn/chess/states/ShowStatsState.java
@@ -1,7 +1,9 @@
 package com.grnn.chess.states;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.PolygonSpriteBatch;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -22,6 +24,8 @@ public class ShowStatsState extends State {
     private int xPos;
     private int yPos;
     private PlayerData playerData;
+    private BitmapFont fontText;
+    private String text;
 
     public ShowStatsState (GameStateManager gsm, Player currentPlayer, PlayerData playerData) {
         super(gsm);
@@ -32,10 +36,13 @@ public class ShowStatsState extends State {
         this.currentPlayer = currentPlayer;
         background = new Texture("Graphics/Menu/Menu_background.png");
         xPos = 690;
-        yPos = 200;
+        yPos = 130;
         menuButton = new TextButton("Tilbake", skin);
         menuButton.setPosition((float) (xPos - menuButton.getWidth()*1.08), yPos - menuButton.getHeight() - 25);
 
+        fontText = new BitmapFont();
+        fontText.setColor(Color.BLACK);
+       // text = playerData.getTopTenPlayers().get(0).getName();
         stage.addActor(menuButton);
     }
 
@@ -55,6 +62,37 @@ public class ShowStatsState extends State {
     public void render(SpriteBatch sb) {
         sb.begin();
         sb.draw(background, 0,0);
+
+        for(int i = 0; i < 10; i++){
+
+        }
+
+        fontText.draw(sb, playerData.getTopTenPlayers().get(0).getName(), 420, 430);
+        fontText.draw(sb, playerData.getTopTenPlayers().get(1).getName(), 420, 400);
+        fontText.draw(sb, playerData.getTopTenPlayers().get(2).getName(), 420, 370);
+        fontText.draw(sb, playerData.getTopTenPlayers().get(3).getName(), 420, 340);
+        fontText.draw(sb, playerData.getTopTenPlayers().get(4).getName(), 420, 310);
+        fontText.draw(sb, playerData.getTopTenPlayers().get(5).getName(), 420, 280);
+        fontText.draw(sb, playerData.getTopTenPlayers().get(6).getName(), 420, 250);
+        fontText.draw(sb, playerData.getTopTenPlayers().get(7).getName(), 420, 230);
+        fontText.draw(sb, playerData.getTopTenPlayers().get(8).getName(), 420, 200);
+        fontText.draw(sb, playerData.getTopTenPlayers().get(9).getName(), 420, 170);
+
+
+        fontText.draw(sb, "" + playerData.getTopTenPlayers().get(0).rating, 600, 430);
+        fontText.draw(sb, "" + playerData.getTopTenPlayers().get(1).rating, 600, 400);
+        fontText.draw(sb, "" + playerData.getTopTenPlayers().get(2).rating, 600, 370);
+        fontText.draw(sb, "" + playerData.getTopTenPlayers().get(3).rating, 600, 340);
+        fontText.draw(sb, "" + playerData.getTopTenPlayers().get(4).rating, 600, 310);
+        fontText.draw(sb, "" + playerData.getTopTenPlayers().get(5).rating, 600, 280);
+        fontText.draw(sb, "" + playerData.getTopTenPlayers().get(6).rating, 600, 250);
+        fontText.draw(sb, "" + playerData.getTopTenPlayers().get(7).rating, 600, 230);
+        fontText.draw(sb, "" + playerData.getTopTenPlayers().get(8).rating, 600, 200);
+        fontText.draw(sb, "" + playerData.getTopTenPlayers().get(9).rating, 600, 170);
+
+
+
+
         sb.end();
         stage.draw();
     }
